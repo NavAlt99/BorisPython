e384ff3f3342a9536f02af9721352a42356dd3d3
NavD <naveen_netb@yahoo.com>
2023-01-26 18:25:09 +0530

    range function

---
+++ learn-to-code-with-python-incomplete/10-Lists-Iteration/5. the-range-function.py
@@ -0,0 +1,12 @@
+# for number in range(10):
+#     print(number)
+
+# for number in range(3, 9):
+#     print(number)
+
+# for number in range(10, 101, 8):
+#     print(number)
+
+
+for numr in range(99, -1, -11):
+    print(numr)
