1bf8ebc6ee7fc887d69d5677e6ed0e6e5a69eb65
NavD <naveen_netb@yahoo.com>
2023-01-30 16:28:30 +0530

    commiting new UN

---
+++ learn-to-code-with-python-incomplete/10-Lists-Iteration/7. the-continue-keyword.py
@@ -0,0 +1,26 @@
+# def sum_of_positivie_numbers(values):
+#     total = 0
+
+#     for val in values:
+#         if val > 0:
+#             total += val
+#     return total
+
+def sum_of_positivie_numbers(values):
+    total = 0 
+
+    for val in values:
+        if val < 0:
+            continue
+
+        total += val
+    
+    return total
+
+
+
+print(sum_of_positivie_numbers([1, 2, -3, 4]))
+
+
+
+aajumbare
