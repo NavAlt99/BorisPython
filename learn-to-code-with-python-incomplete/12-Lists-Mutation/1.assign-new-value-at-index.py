12899dc82a946552d218d6a5c66e90f1e2aa22a6
NavD <naveen_netb@yahoo.com>
2023-02-17 23:38:06 +0530

    some vscode glitch

---
+++ learn-to-code-with-python-incomplete/12-Lists-Mutation/1.assign-new-value-at-index.py
@@ -0,0 +1,16 @@
+# Assign new value to index possition 
+
+crayons = ["Macaroni and cheese", "Maximum Yellow Red", "Jazzberry Jam"]
+print(crayons)
+
+
+crayons[1] = "Cotten Candy" 
+print(crayons)
+
+crayons[0] = "Blizzard Blue"
+print(crayons)
+
+crayons[-1] = "Tiktok Blue"
+print(crayons)
+
+
---
+++ learn-to-code-with-python-incomplete/12-Lists-Mutation/2.assign-new-values-to-list-slice.py
@@ -0,0 +1,18 @@
+#Assign New Values to List Slice
+
+coworkers = ["Chinna", "Surendra", "Mahendra", "Ramya", "Hari",  "Sushma", "Raghav"]
+
+# coworkers[4:6] = ["Shaheena", "Prabhuteja"]
+# print(coworkers)
+
+# coworkers[4:6] = ["Shaheena"]
+# print(coworkers)
+
+
+# coworkers[4:6] = ["Shaheena", "Prabhuteja","Nagababu"]
+# print(coworkers)
+
+
+coworkers[-5:-1] = ["Ryan", "Andy"]
+print(coworkers)
+
