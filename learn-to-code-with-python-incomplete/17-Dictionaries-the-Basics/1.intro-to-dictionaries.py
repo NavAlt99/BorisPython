f0579d287a1eba49ccaa49a1264433602a76fde1
Naveen <naveen_netb@yahoo.com>
2023-05-05 07:31:21 +0530

    May52023

---
+++ learn-to-code-with-python-incomplete/17-Dictionaries-the-Basics/1.intro-to-dictionaries.py
@@ -0,0 +1,8 @@
+ice_cream_prefernces = {
+    "Benjamin": "Chocolate",
+    "Naveen": "Arun grape lickstick",
+    "Mona": "Arun Orange lickstick",
+    "Mom": "Butter Scotch" 
+}
+
+print(len(ice_cream_prefernces))
