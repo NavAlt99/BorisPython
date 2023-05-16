bbf39161a17d3e769ed3007f4f4861a63a41538a
NavAlt99 <naveen_netb@yahoo.com>
2023-05-16 19:13:40 +0530

    second commit

---
+++ learn-to-code-with-python-incomplete/17-Dictionaries-the-Basics/2.access-a-dictionary-value-by-key-or-the-get-method.py
@@ -0,0 +1,10 @@
+flight_prices = { 
+    "Chicago": 199,
+    "San Francisco": 499,
+    "Denver": 295,
+    "San Andreas": 500
+}
+
+
+print(flight_prices["Chicago"])
+print(flight_prices["Denver"])
