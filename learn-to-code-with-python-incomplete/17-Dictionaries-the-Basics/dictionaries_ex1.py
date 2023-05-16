ead347e8c0c9e22bafcdad1a5347ff70629ef128
NavAlt99 <naveen_netb@yahoo.com>
2023-05-16 18:31:56 +0530

    From_linux_machine

---
+++ learn-to-code-with-python-incomplete/17-Dictionaries-the-Basics/dictionaries_ex1.py
@@ -0,0 +1,25 @@
+# Create an empty dictionary and assign it to the variable empty.
+
+empty = {}
+
+
+# Create a dictionary with three key-value pairs. 
+# The keys should be strings and the values should be integer values. 
+# Assign the dictionary to a my_dict variable.
+
+my_dict = {
+    "key1" : 400,
+    "key2" : 500,
+    "key3" : 500
+}
+
+# A dictionary’s keys can be any immutable data structure. 
+# Create a dictionary with two key-value pairs and assign it to
+# a winning_lottery_numbers variable. 
+# Both of the keys should be tuples. 
+# One of the values should be True, the other value should be False.
+
+winning_lottery_numbers = { 
+    (4, 8, 15, 16, 23, 42): True,
+    (6, 12, 14): False
+}
